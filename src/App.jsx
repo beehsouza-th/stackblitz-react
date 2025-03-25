45f25f73c83faf0362d4d165d9c81fff51bfcf0b
Brenda Souza <brenda.souza@estudante.ifms.edu.br>
2025-03-25 15:28:12 -0400

    página login

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,69 @@
 import { useState } from 'react';
-import {auth} from './config/firebaseConfig.js';
-import {singInwithEmailAndPssword} from 'firebase/auth';
+import { auth } from './config/firebaseConfig.js';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import './App.css/styles.css'
 
 export default function App() {
- const [email, setEmail] = useState('');
- const [senha, setSenha] = useState('');
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
 
- const autenticarComFirebase = async (evento) =>{
+  const autenticarComFirebase = async (evento) => {
     evento.preventDefault();
-    try{
-      await singInwithEmailAndPssword(auth, email, senha);
+    try {
+      await signInWithEmailAndPassword(auth, email, senha);
       alert('Logado com sucesso!');
-    }catch (err){
+    } catch (err) {
       alert('Erro no processo', err);
-    } 
- };
- return(
-  <main>
-    <form onSubmit={autenticarComFirebase}>
-      <label htmlFor='email'>E-mail:</label>
-      <input 
-      id="email"
-      nome="email"
-      type="email"
-      value={email}
-      onChange={(evento)=> setEmail(evento.target.value)}
-      />
-      <label htmlFor='password'>Senha:</label>
-      <input
-      id="password"
-      nome="password"
-      type="password"
-      value={senha}
-      onChange={(evento)=> setSenha(evento.target.value)}
-      />
-      <button onClick={autenticarComFirebase}></button>
-    </form>
-  </main>
- );
+    }
+  };
+  return (
+    <main>
+      <div className='container'>
+        <div className='form-image'>
+          <img src= 'assets/img/undraw_login_weas.svg' />
+        </div>
+        <div className='form'>
+         <form onSubmit={autenticarComFirebase()}> 
+            <div className='form-header'>
+              <div className='title'>
+                <h1>Login</h1>
+              </div>
+              <div className='input-group'>
+                <div className='input-box'>
+                <label htmlFor="email">E-mail:</label>
+              <input
+                 id="email"
+                 name="e-mail"
+                 placeholder='Ex:jorge@gmail.com'
+                 type="email"
+                 value={email}
+                 onChange={(evento) => setEmail(evento.target.value)}
+              />
+              </div>
+
+              
+               <div className="input-box">
+               <label htmlFor="password">Senha:</label>
+              <input
+                id="password"
+                name="password"
+                placeholder='Digite sua senha'
+                type="password"
+                value={senha}
+                onChange={(evento) => setSenha(evento.target.value)}
+              />
+              </div>
+              
+              <div className='login-button'>
+              <button type="submit"> Fazer Login </button>
+              </div>
+
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </main>
+  
+  );
 }
-
-
