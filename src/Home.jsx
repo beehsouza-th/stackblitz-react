e6972e15e661285b76100e201e942369c5ee188a
Brenda Souza <brenda.souza@estudante.ifms.edu.br>
2025-04-23 18:35:31 -0400

    home link

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,12 @@
+import { Link } from 'react-router-dom';
 import './App.css/styles.css'
 export default function Home(){
     return(
         <main>
             <h1>Bem vindo!</h1>
+            <Link className='button-register' to= "/login">
+                <h2>Voltar para a página de login</h2>
+            </Link>
         </main>
     );
 }
